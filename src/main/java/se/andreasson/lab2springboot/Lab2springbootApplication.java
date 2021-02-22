7e685c063a2e2853877e56e39a01f763d8e48569
Petra Andreasson <petra.andreasson@iths.se>
2021-02-22 08:16:31 +0100

    Commit clean repo after .gitignore

---
+++ src/main/java/se/andreasson/lab2springboot/Lab2springbootApplication.java
@@ -0,0 +1,13 @@
+package se.andreasson.lab2springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2springbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab2springbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/andreasson/lab2springboot/Lab2springbootApplicationTests.java
@@ -0,0 +1,13 @@
+package se.andreasson.lab2springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2springbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
